34cc60f15f61728a0cbd4a3b785e92b3d9208947
SVRourke <samuelvanrourke@gmail.com>
2021-10-01 17:41:35 -0400

    stubbed out controllers

---
+++ controllers/authorController.js
@@ -0,0 +1,26 @@
+var Author = require("../models/author");
+
+exports.author_list = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_detail = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_create_get = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_create_post = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_delete_get = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_delete_post = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_update_get = function (req, res) {
+  res.send("Not Implemented");
+};
+exports.author_update_post = function (req, res) {
+  res.send("Not Implemented");
+};
